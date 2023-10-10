7a31281724643f063d96f048b42b2b47b345ee57
kim-limit <rlawogks2468@naver.com>
2023-10-10 18:05:45 +0900

    feat: 라우터 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,20 @@
+import { RouterProvider } from 'react-router-dom';
+
+import { ThemeProvider } from '@emotion/react';
+
+import { router } from './routes/router';
+import { GlobalStyle } from './styles/globalStyle';
+import { theme } from './styles/theme';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
